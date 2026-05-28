d7e539713b44c491d8f93526fd4ea57b72274f9f
erics118 <52634785+erics118@users.noreply.github.com>
2026-05-27 23:05:30 -0400

    move espanso and fix it

---
+++ modules/eric/desktop/espanso.nix
@@ -0,0 +1,22 @@
+{
+  flake.modules.homeManager.base =
+    { repoFile, lib, ... }:
+    let
+      managedFiles = [
+        "accents"
+        "keys"
+        "latex"
+        "typography"
+      ];
+    in
+    {
+      # live-symlinks so edits to espanso match files take effect without a rebuild
+      home.file =
+        lib.listToAttrs (
+          map (name: {
+            name = ".config/espanso/match/managed_${name}.yml";
+            value.source = repoFile "modules/eric/desktop/espanso/${name}.yml";
+          }) managedFiles
+        );
+    };
+}
