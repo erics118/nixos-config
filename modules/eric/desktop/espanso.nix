28a06a30858d1f2ae6f0283ccef5de7663ebfbac
erics118 <52634785+erics118@users.noreply.github.com>
2026-05-28 02:41:02 -0400

    misc

--- modules/eric/desktop/espanso.nix
+++ modules/eric/desktop/espanso.nix
@@ -11,12 +11,11 @@
     in
     {
       # live-symlinks so edits to espanso match files take effect without a rebuild
-      home.file =
-        lib.listToAttrs (
-          map (name: {
-            name = ".config/espanso/match/managed_${name}.yml";
-            value.source = repoFile "modules/eric/desktop/espanso/${name}.yml";
-          }) managedFiles
-        );
+      home.file = lib.listToAttrs (
+        map (name: {
+          name = ".config/espanso/match/managed_${name}.yml";
+          value.source = repoFile "modules/eric/desktop/espanso/${name}.yml";
+        }) managedFiles
+      );
     };
 }
---
+++ modules/eric/xdg.nix
@@ -0,0 +1,10 @@
+{
+  flake.modules.homeManager.base = {
+    home.sessionVariables = {
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_STATE_HOME = "$HOME/.local/state";
+    };
+  };
+}
