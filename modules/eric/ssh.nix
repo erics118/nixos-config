591bde71ad9f50c716b4f61cd501dcbc1434f582
erics118 <52634785+erics118@users.noreply.github.com>
2026-07-09 00:28:43 -0400

    prevent 1pass from taking over everything

--- modules/eric/ssh.nix
+++ modules/eric/ssh.nix
@@ -46,6 +46,7 @@
             controlMaster = "no";
             controlPath = "~/.ssh/master-%n-%C";
             controlPersist = "no";
+            identitiesOnly = true;
             identityAgent = lib.mkIf isDarwin "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
           };
         };
