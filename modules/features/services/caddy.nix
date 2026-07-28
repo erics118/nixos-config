79eb7499af6ef6de01a56f70e9da721289e672cd
erics118 <52634785+erics118@users.noreply.github.com>
2026-07-27 17:38:22 -0700

    update caddy hash

--- modules/features/services/caddy.nix
+++ modules/features/services/caddy.nix
@@ -19,7 +19,7 @@
       # we need to manually specify the plugin and hash
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+        hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
       };
 
       globalConfig = ''
