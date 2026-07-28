67fd3c5b533698fccf029a9537f983c6534ebafd
erics118 <52634785+erics118@users.noreply.github.com>
2026-07-27 17:44:27 -0700

    Revert "see if we acn disable glances test overlays"
    This reverts commit 6203d83539111823dad19031a0584c483982714f.

--- modules/overlays/glances.nix
+++ modules/overlays/glances.nix
@@ -1,13 +1,13 @@
 {
-  # flake.overlays.glances = _: prev: {
-  #   glances = prev.glances.overrideAttrs (old: {
-  #     # test_phys_core_returns_int is broken
-  #     disabledTests = (old.disabledTests or [ ]) ++ [ "test_phys_core_returns_int" ];
-  #     # these spin up a local http server and flakily fail to connect in the sandbox
-  #     disabledTestPaths = (old.disabledTestPaths or [ ]) ++ [
-  #       "tests/test_restful.py"
-  #       "tests/test_browser_restful.py"
-  #     ];
-  #   });
-  # };
+  flake.overlays.glances = _: prev: {
+    glances = prev.glances.overrideAttrs (old: {
+      # test_phys_core_returns_int is broken
+      disabledTests = (old.disabledTests or [ ]) ++ [ "test_phys_core_returns_int" ];
+      # these spin up a local http server and flakily fail to connect in the sandbox
+      disabledTestPaths = (old.disabledTestPaths or [ ]) ++ [
+        "tests/test_restful.py"
+        "tests/test_browser_restful.py"
+      ];
+    });
+  };
 }
