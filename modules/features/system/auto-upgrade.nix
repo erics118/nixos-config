d0ce0317dc288b2b23f2d581761777a5af75cbbd
erics118 <52634785+erics118@users.noreply.github.com>
2026-07-30 16:55:57 -0400

    fix user

--- modules/features/system/auto-upgrade.nix
+++ modules/features/system/auto-upgrade.nix
@@ -9,7 +9,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
 
       extraConfig = ''
-        Match user root host github.com
+        Match localuser root host github.com
           IdentityFile /root/.ssh/id_deploy
           IdentitiesOnly yes
       '';
