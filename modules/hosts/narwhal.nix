e4a10d3ccd9c87987dd312ce18d6ac40c55eb068
erics118 <52634785+erics118@users.noreply.github.com>
2026-08-02 15:33:24 -0400

    reduce narwhal configurationLimit

--- modules/hosts/narwhal.nix
+++ modules/hosts/narwhal.nix
@@ -63,7 +63,7 @@ in
       loader = {
         systemd-boot = {
           enable = true;
-          configurationLimit = 5;
+          configurationLimit = 3;
           consoleMode = "max";
         };
         efi.canTouchEfiVariables = true;
