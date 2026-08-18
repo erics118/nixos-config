5cdfccfa5690e7c66e1073e1b3a7f3928ed53215
erics118 <52634785+erics118@users.noreply.github.com>
2026-08-18 17:43:04 -0400

    fix lazygit again

--- modules/eric/cli/dev-tools.nix
+++ modules/eric/cli/dev-tools.nix
@@ -1,35 +1,47 @@
 {
-  flake.modules.homeManager.base = {
-    programs.direnv = {
-      enable = true;
-      enableZshIntegration = false; # pre-computed in shell.nix
-      nix-direnv.enable = true;
-      config = {
-        global = {
-          load_dotenv = true;
-          strict_env = true;
-          hide_env_diff = true;
+  flake.modules.homeManager.base =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      programs.direnv = {
+        enable = true;
+        enableZshIntegration = false; # pre-computed in shell.nix
+        nix-direnv.enable = true;
+        config = {
+          global = {
+            load_dotenv = true;
+            strict_env = true;
+            hide_env_diff = true;
+          };
         };
       };
-    };
 
-    programs.lazygit = {
-      enable = true;
+      programs.lazygit = {
+        enable = true;
 
-      settings = {
-        gui = {
-          nerdFontsVersion = "3";
-          filterMode = "fuzzy";
-          showRandomTip = false;
+        settings = {
+          gui = {
+            nerdFontsVersion = "3";
+            filterMode = "fuzzy";
+            showRandomTip = false;
+          };
+          git.diffRenderers = [
+            {
+              colorArg = "always";
+              command = "delta --paging=never";
+            }
+          ];
+          update.method = "never";
         };
-        git.diffRenderers = [
-          {
-            colorArg = "always";
-            command = "delta --paging=never";
-          }
-        ];
-        update.method = "never";
       };
+
+      # lazygit on macOS reads ~/Library/Application Support/lazygit/config.yml, but home-manager writes to ~/.config/lazygit/config.yml
+      home.file."Library/Application Support/lazygit/config.yml" =
+        lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+          { inherit (config.home.file."${config.xdg.configHome}/lazygit/config.yml") source enable; };
     };
-  };
 }
